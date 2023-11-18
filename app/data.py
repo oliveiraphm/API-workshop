3079207b99aac3fd4c6685be2ce647377351d6d4
Paulo Oliveira <oliveiraphm@gmail.com>
2023-11-18 18:23:24 -0300

    sei la

---
+++ app/data.py
@@ -0,0 +1,20 @@
+produtos: List[Dict[str, Any]] = [
+    {
+        "id": 1,
+        "titulo": "Cadeira Gamer",
+        "descricao": "Cadeira confortável para fazer live",
+        "preço": 5.0,
+    },
+    {
+        "id": 2,
+        "a titulo": "Workshop",
+        "descricao": "Workshop de deploy",
+        "preço": 100.00,
+    },
+    {
+        "id": 3,
+        "a titulo": "Iphone",
+        "descricao": "Iphone 14",
+        "preço": None,
+    },
+]
